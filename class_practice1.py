ad344a532ce652866b90ee1be056056b228bf19b
Amresh04 <kumaramresh0174@gmail.com>
2026-06-17 14:38:27 +0530

    python

---
+++ class_practice1.py
@@ -0,0 +1,8 @@
+class programmer:
+    company="microsoft"
+    def __init__(self,name,salary,pincode):
+        self.name=name
+        self.salary=salary
+        self.pincode=pincode
+p=programmer("Amresh",1200000,847107)   
+print(p.name,p.salary,p.pincode,p.company)     
