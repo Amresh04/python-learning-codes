7b1c49c83b650b0706fb91af2f9bf5d3d074ddaf
Amresh04 <kumaramresh0174@gmail.com>
2026-06-18 08:03:03 +0530

    python

---
+++ class_practice3.py
@@ -0,0 +1,7 @@
+class demo:
+    a=4
+o=demo()
+print(o.a)    #prints the class attributes because instence attribute is not present 
+o.a=0
+print(o.a)#prints the class attributes because instence attribute is n present 
+print(demo.a)
