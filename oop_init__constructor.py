9820d82fc6a255ffc823b14fa7678784e44e4802
Amresh04 <kumaramresh0174@gmail.com>
2026-06-26 17:57:55 +0530

    opps

---
+++ oop_init__constructor.py
@@ -0,0 +1,9 @@
+class employee:
+    language="python"
+    salary=1200000
+    def __init__(self):
+        print("i am creating object")
+  
+amresh=employee()
+amresh.name="amresh"
+print(amresh.name,amresh.salary)
