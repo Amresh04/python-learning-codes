6430b677b516ab5e11183152371dd4977f6f92e9
Amresh04 <kumaramresh0174@gmail.com>
2026-06-26 17:59:29 +0530

    oops

---
+++ 02_oops_practice.py
@@ -0,0 +1,17 @@
+class calculator:
+    def __init__(self,n):
+      self.n=n
+    def square(self):
+       print("square=",self.n*self.n)
+    def cube(self):
+       print("cube=",self.n*self.n*self.n)  
+    def squareroot(self):
+       print("squareroot=",(self.n**0.5))
+
+
+a=float(input("enter the no:"))
+result=calculator(a)
+result.square()
+result.cube()
+result.squareroot()
+
